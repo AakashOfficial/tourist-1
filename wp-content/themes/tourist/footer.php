5219702ffba55645308260d74f2d8964cfa08562
brineids <bhneiderman@gmail.com>
2017-07-13 23:21:15 -0500

    tourist site draft 2

--- wp-content/themes/tourist/footer.php
+++ wp-content/themes/tourist/footer.php
@@ -14,13 +14,16 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-			<h3>Tourist New App</h3>
-			<p>Support: <a href="mailto:support@TouristNewApp.com">support@TouristNewApp.com</a></p>
-			<p><a href="http://touristnewapp.com/terms_privacy.html">Terms</a> and <a href="http://touristnewapp.com/privacy.html">Privacy Policy</a></p>
-			<p>&#169 2016-2099 Pine Group, LLC. All rights reserved.</p>
-		</div><!-- .site-info -->
+        <a class="footer-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img class="logo" src= "http://localhost:8888/tourist/wp-content/uploads/2017/07/tourist-logo-e1499271214549.png" />
+            <span>ourist New App</span>
+        </a>
+        <p>Support: <a href="mailto:support@TouristNewApp.com">support@TouristNewApp.com</a></p>
+        <p><a href="http://touristnewapp.com/terms_privacy.html">Terms</a> and <a href="http://touristnewapp.com/privacy.html">Privacy Policy</a></p>
+        <div class="ganyc-logo"><a href="http://www.ganyc.org/industry-partners" target="_black"><img src="http://localhost:8888/tourist/wp-content/uploads/2017/07/ganyc_logo.png" alt="GANYC" /></a></div>
+        <p>&#169 2016-2099 Pine Group, LLC. All rights reserved.</p>
 	</footer><!-- #colophon -->
+    </div><!-- .site-info -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
